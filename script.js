ea430ae11a0f0fb1d82156613c779f5c33d2b4cd
Ladan <lada0080@stud.kea.dk>
2024-01-08 21:47:52 +0100

    js, burgermenu, gbl nav menu

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const menu = document.querySelector(".burger-nav");
+const burger = document.querySelector(".burger-menu");
+const close = document.querySelector(".kryds");
 
+burger.addEventListener("click", toggleMenu);
+close.addEventListener("click", toggleMenu);
+function toggleMenu() {
+  menu.classList.toggle("active");
+  burger.classList.toggle("cross");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var burgerIcon = document.getElementById("burger-icon");
+  var menu = document.querySelector(".menu");
+
+  burgerIcon.addEventListener("click", function () {
+    menu.classList.toggle("active");
+  });
+});
